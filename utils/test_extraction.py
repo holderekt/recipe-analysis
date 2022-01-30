ee03ae9c3702b303aae5733a593dfa803e7c0e01
Ivan Diliso <diliso.ivan@gmail.com>
2022-01-30 10:49:58 -0800

    Estrazione primi 100 elementi

---
+++ utils/test_extraction.py
@@ -0,0 +1,12 @@
+import csv
+from pathlib import Path
+from itertools import islice
+
+with open(Path("data/RecipeNLG_dataset.csv").resolve(), "r") as csv_file:
+    reader = csv.reader(csv_file)
+    data = [element for element in islice(reader, 2, 100)]
+
+with open(Path("data/test_dataset.csv").resolve(), "w") as csv_file:
+    writer = csv.writer(csv_file)
+    for element in data:
+        writer.writerow(element)
