db050ac014cea730c758fa3dd7ae1f8b4a3c143f
mavolio1 <marcoavolio08@gmail.com>
2022-02-06 20:43:08 +0100

    Portato dataset a 2000, fixati problemi minori

--- utils/test_extraction.py
+++ utils/test_extraction.py
@@ -4,7 +4,7 @@ from itertools import islice
 
 with open(Path("data/RecipeNLG_dataset.csv").resolve(), "r") as csv_file:
     reader = csv.reader(csv_file)
-    data = [element for element in islice(reader, 2, 100)]
+    data = [element for element in islice(reader, 1, 2001)]
 
 with open(Path("data/test_dataset.csv").resolve(), "w") as csv_file:
     writer = csv.writer(csv_file)
